f0a08e1ed36a345c9851751d7464ff22537d0b57
Jasper <jasperutilities@fastmail.com>
2025-12-20 15:34:09 -0500

    newCNAME

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 export default function(eleventyConfig) {
     eleventyConfig.setInputDirectory("src")
     eleventyConfig.addPassthroughCopy({ "src/assets": "assets" })
+    eleventyConfig.addPassthroughCopy("CNAME")
     eleventyConfig.setDataFileSuffixes([ ".data" ])
 }
